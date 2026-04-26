5929d867bd9d1dd02df778db68c375b0464a8098
Ronellyn Soniel <sonielronellyn25@gmail.com>
2026-04-25 17:10:08 -1200

    Homepage app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import ProductList from './ProductList';
 import Homepage from './app/main/homepage/page';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
