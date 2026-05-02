503169444f90057f6cf0f9361b426b5212b9c02d
Ronellyn Soniel <sonielronellyn25@gmail.com>
2026-05-02 11:07:48 -1200

    update index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Homepage from './app/main/homepage/page';
+import swDev from './swDev';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,3 +12,4 @@ root.render(
 );
 
 reportWebVitals();
+swDev();
